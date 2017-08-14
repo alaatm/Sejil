b34edc53ffeb5bae672473b6827d4d0c972d90a1
Alaa Masoud <alaa.masoud@live.com>
2017-08-14 15:01:25 +0300

    Fix sln file

--- src/Sejil.Server/WebHostBuilderExtensions.cs
+++ src/Sejil.Server/WebHostBuilderExtensions.cs
@@ -2,7 +2,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
