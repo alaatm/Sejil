bd638e1e6b9192643598cd479dd7b2f2eeafa485
Wenzel, Toni <toni.wenzel@chg-meridian.com>
2019-01-03 08:14:41 +0100

    Initial try

---
+++ sample/Sample2.0/Authentication/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sample.Authentication
+{
+    public interface IUserService
+    {
+        Task<User> Authenticate(string username, string password);
+        Task<IEnumerable<User>> GetAll();
+    }
+}
