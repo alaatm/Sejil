936e8838a0deb85ff0917d4311a3bd03256b8de2
Alaa Masoud <alaa.masoud@live.com>
2019-08-30 08:32:27 +0300

    Fall back to error when message is null

--- src/Sejil.Client/src/Store.ts
+++ src/Sejil.Client/src/Store.ts
@@ -73,7 +73,7 @@ export default class Store {
         } catch (err) {
             runInAction('set loading finished', () => this.loading = false);
             if (this.onEventsLoadError) {
-                this.onEventsLoadError(err);
+                this.onEventsLoadError(err.message || err);
             }
         }
     }
